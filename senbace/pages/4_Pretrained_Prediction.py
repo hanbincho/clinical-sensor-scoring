e6e17d13484522a72a1424fafb66c9f2009c64f1
Hanbin Cho <hanbincho4261@gmail.com>
2022-12-07 14:54:14 -0800

    Create directories if they do not exist

--- senbace/pages/4_Pretrained_Prediction.py
+++ senbace/pages/4_Pretrained_Prediction.py
@@ -29,7 +29,7 @@ with col1:
     user_batch_size = st.text_input("Batch Size", 1)
 
     # Button to start prediction
-    st.button("Predict Scores")
+    predict_pressed = st.button("Predict Scores")
 
 with col2:
     # Text input box for feedback
@@ -37,25 +37,34 @@ with col2:
 
 if image_file is not None:
     st.write("Loaded: ", image_file.name)
-    # Download uploaded image in current working directory
-    download_path = os.getcwd()+"/data/download_image/"
+    # Check that download directory for image exists
+    download_image_path = os.getcwd()+"/data/"
+    if not os.path.exists(download_image_path):
+        # If not, make it
+        os.makedirs(download_image_path)
+    # Then save the image
     img_to_save = Image.open(image_file)
-    img_to_save.save(download_path+image_file.name)
+    img_to_save.save(download_image_path+image_file.name)
 
 
 if model_file is not None:
     st.write("Loaded: ", model_file.name)
-    # Download uploaded model in current working directory
-    download_model_path = os.getcwd()+"/data/download_model/"+model_file.name
+    # Check that download directory for model exists
+    download_model_path = os.getcwd()+"/model/"
+    if not os.path.exists(download_model_path):
+        # If not, make it
+        os.makedirs(download_model_path)
+    # Then save the model file
     model_data = model_file.getvalue()
-    model_to_save = open(download_model_path, "wb")
+    model_to_save = open(download_model_path+model_file.name, "wb")
     model_to_save.write(model_data)
     model_to_save.close()
+    
 
 # Perform prediction with pretrained model on uploaded image
-if st.button:
-    data_loader = load_data(download_path, int(user_batch_size))
-    pred_score = score_prediction(data_loader, download_model_path)
+if predict_pressed:
+    data_loader = load_data(download_image_path, int(user_batch_size))
+    pred_score = score_prediction(data_loader, download_model_path+model_file.name)
     for i in range(len(pred_score)):
         st.write("Result for "+ image_file.name + ": " + str(pred_score[i]))
         st.image(img_to_save)

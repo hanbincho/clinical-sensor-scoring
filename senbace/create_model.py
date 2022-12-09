938e31530ecfdd0712ebafc66704033496dcc3ef
Hanbin Cho <hanbincho4261@gmail.com>
2022-12-09 15:44:56 -0800

    Fix indent errors

--- senbace/create_model.py
+++ senbace/create_model.py
@@ -16,8 +16,8 @@ def create_model(model_name):
         DESCRIPTION.
 
     """
-  if model_name == "AlexNet":
-    # create model
-    model = AlexNet(num_classes=7) # since score can range from 0-7
-    model.to(curr_device) # move the model to GPU
+    if model_name == "AlexNet":
+        # create model
+        model = AlexNet(num_classes=7) # since score can range from 0-7
+        model.to(curr_device) # move the model to GPU
     return model
